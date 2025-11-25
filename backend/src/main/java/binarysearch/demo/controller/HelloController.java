97f2f8537fe90d9093142c32136d63a4236fa70b
caduhidee <c.hyde1117@gmail.com>
2025-11-25 16:26:33 -0500

    fixed frontend to be part of main repo

---
+++ backend/src/main/java/binarysearch/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package binarysearch.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Binary Search!";
+    }
+}
